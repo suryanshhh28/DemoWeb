be66356c17d1c4ad0ffd5fe361088b9d82af6966
Suryansh Chaudhary <suryanshchaudhary28@gmail.com>
2023-12-13 19:23:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Affordable from "./components/Affordable";
+import BestAgency from "./components/BestAgency";
+import Carousel from "./components/Carousel";
+import ContactHeader from "./components/ContactHeader";
+import Header from "./components/Header";
+import Information from "./components/Information";
+import LatestFromBlogs from "./components/LatestFromBlogs";
+import SignUpEmail from "./components/SignUpEmail";
+import Testimonials from "./components/Testimonials";
+import Footer from "./pages/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ContactHeader />
+      <Header />
+      <Carousel />
+      <BestAgency />
+      <Information />
+      <Testimonials />
+      <LatestFromBlogs />
+      <SignUpEmail />
+      <Affordable />
+      <Footer />
     </div>
   );
 }
